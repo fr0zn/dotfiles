b02af492c8126b154e84cf43991e4673cb006f80
Ferran Celades <ferrancelades@hotmail.es>
2017-04-13 20:44:33 +0200

    Added readme, fixed install, pointing to the new repo

---
+++ README.md
@@ -0,0 +1,13 @@
+# e0d1n dotfiles
+
+![](https://cdn.pbrd.co/images/3TkxW3OKd.png)
+
+* tmux
+* zsh (Oh my zsh!)
+* vim
+* docker (pwnbox)
+
+# Installation
+
+bash <(curl -L http://bit.ly/2pbpz5R)
+
--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-$DOTFILE_REPO=https://github.com/jimeh/tmux-themepack.git
+$DOTFILE_REPO=https://github.com/e0d1n/dotfiles.git
 $DOTFILE_DESTINATION=$HOME/e0d1n-dotfiles
 
 msg() {
@@ -86,9 +86,13 @@ clone https://github.com/jimeh/tmux-themepack.git $HOME/.tmux-themepack
 
 # Set zsh default
 chsh -s /bin/zsh
+
 # Install Oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# Install vim-plug
+curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 symlink "$HOME/.vimrc"
 symlink "$HOME/.tmux.conf"
