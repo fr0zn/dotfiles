138157f437ed13617b6c543576f3b9e8499017f9
fr0zn <fr0zn@protonmail.com>
2018-02-20 19:09:43 +0100

    Fixed tmux symlink step

--- tmux/install.sh
+++ tmux/install.sh
@@ -2,11 +2,11 @@ backup_tmux(){
     backup_file "$HOME/.tmux.conf"
 }
 
+symlink_tmux(){
+    symlink_file "tmux/tmux.conf" "$HOME/.tmux.conf"
+}
+
 install_tmux(){
     install_package "tmux"
 }
 
-symlink_tmux(){
-    program_must_exist "tmux"
-    symlink_file "tmux/tmux.conf" "$HOME/.tmux.conf"
-}
