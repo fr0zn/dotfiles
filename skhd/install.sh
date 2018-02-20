5e612e131c0eef9a8e095bacc40ec0da80fca19a
fr0zn <fr0zn@protonmail.com>
2018-02-20 12:02:57 +0100

    Backup and sym skhd

--- skhd/install.sh
+++ skhd/install.sh
@@ -1,5 +1,13 @@
+backup_skhd_macos(){
+    backup_file "$HOME/.skhdrc"
+}
+
 install_skhd_macos(){
     install_package koekeishiya/formulae/skhd
+}
+
+symlink_skhd_macos(){
+    symlink_file "skhd/skhdrc" "$HOME/.skhdrc"
 }
 
 post_skhd_macos(){
