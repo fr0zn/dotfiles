4851996ae5787b2bcad5569f76046bd169f6decb
fr0zn <fr0zn@protonmail.com>
2018-02-20 19:06:51 +0100

    Tmux installation fixed

--- tmux/install.sh
+++ tmux/install.sh
@@ -3,9 +3,10 @@ backup_tmux(){
 }
 
 install_tmux(){
-    program_must_exist "tmux"
+    install_package "tmux"
 }
 
 symlink_tmux(){
+    program_must_exist "tmux"
     symlink_file "tmux/tmux.conf" "$HOME/.tmux.conf"
 }
