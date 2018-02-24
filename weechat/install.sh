235f2a4973cdcc1c2656b1450f14c363f37cb5fc
fr0zn <ferrancelades@hotmail.es>
2018-02-24 17:34:34 +0100

    Clean weechat installation, db sync force

--- weechat/install.sh
+++ weechat/install.sh
@@ -3,8 +3,8 @@ install_weechat_macos(){
 }
 
 install_weechat_ubuntu(){
-    sudo_run 'echo "deb https://weechat.org/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/weechat.list'
-    sudo_run apt-key adv --keyserver keys.gnupg.net --recv-keys 11E9DE8848F2B65222AA75B8D1820DB22A11534E
-    sudo_run apt-get update
-    install_package weechat weechat-plugins weechat-python weechat-perl
+    clean sudo_run 'echo "deb https://weechat.org/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/weechat.list'
+    clean sudo_run 'apt-key adv --keyserver keys.gnupg.net --recv-keys 11E9DE8848F2B65222AA75B8D1820DB22A11534E'
+    DB_SYNC=0
+    install_package 'weechat weechat-plugins weechat-python weechat-perl'
 }
