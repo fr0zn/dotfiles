e427288278e1fb1145c599eae5668db297b75757
fr0zn <ferrancelades@hotmail.es>
2018-02-25 23:25:33 +0100

    Updated using add_line

--- packages/gef.sh
+++ packages/gef.sh
@@ -1,4 +1,4 @@
 install_gef(){
     clone https://github.com/hugsy/gef.git "$DOTFILE_SRC/gef"
-    echo "$DOTFILE_SRC/gef/gef.py" >> $HOME/.gdbinit
+    add_line "$HOME/.gdbinit" "source $DOTFILE_SRC/gef/gef.py"
 }
