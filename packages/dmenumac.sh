0877a3015ed4f85a5b12da3c2f4b57bf67926ad7
fr0zn <ferrancelades@hotmail.es>
2018-03-01 19:36:27 +0100

    Renamed dmenumac file

--- packages/dmenumac.sh
+++ packages/dmenumac.sh
@@ -1,6 +1,7 @@
 install_dmenumac_macos(){
     if ! is_app_installed "dmenu-mac"; then
         mkdir -p "$DOTFILE_SRC/dmenu-mac"
+        rm "$DOTFILE_SRC/dmenu-mac/dmenu-mac.zip" 2> /dev/null
         curl -L "https://github.com/fr0zn/dmenu-mac/releases/download/0.5/dmenu-mac.zip" > "$DOTFILE_SRC/dmenu-mac/dmenu-mac.zip"
         cd "$DOTFILE_SRC/dmenu-mac"
         unzip "dmenu-mac.zip"
@@ -8,3 +9,7 @@ install_dmenumac_macos(){
     fi
 }
 
+post_dmenumac_macos(){
+    open_app "dmenu-mac"
+    add_app_login "dmenu-mac"
+}
