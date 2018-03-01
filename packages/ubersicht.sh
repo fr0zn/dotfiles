19bdd6a094886b71c3a99d6802023ff056c2e625
fr0zn <ferrancelades@hotmail.es>
2018-03-01 19:36:47 +0100

    Ubersicht installation

--- packages/ubersicht.sh
+++ packages/ubersicht.sh
@@ -5,4 +5,5 @@ install_ubersicht_macos(){
 post_ubersicht_macos(){
      clone https://github.com/fr0zn/i3-statusbar.git "$HOME/Library/Application Support/Übersicht/widgets/i3-statusbar"
      open_app "Übersicht"
+     add_app_login "Übersicht"
 }
