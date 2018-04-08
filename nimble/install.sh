517d758fea1913cbe633b5a8a52dee2c66a76ff9
fr0zn <fr0zn@protonmail.com>
2018-04-08 18:10:17 +0200

    First symlink config

--- nimble/install.sh
+++ nimble/install.sh
@@ -1,9 +1,10 @@
-install_nimble_macos(){
-    install_cask nimble-commander
-}
-
-symlink_nimble_macos(){
+pre_nimble_macos(){
     local conf_path="$HOME/Library/Application Support/Nimble Commander/Config"
     mkdir -p "$conf_path"
     symlink_file "nimble/Config.json" "$conf_path/Config.json"
 }
+
+install_nimble_macos(){
+    install_cask nimble-commander
+}
+
