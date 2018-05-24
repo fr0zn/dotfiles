585e4bd64c847e9705cd529008880e8917c50f7d
fr0zn <fr0zn@protonmail.com>
2018-05-24 09:09:02 +0200

    Updating

--- scripts/pyenv_rbenv_version.sh
+++ scripts/pyenv_rbenv_version.sh
@@ -8,18 +8,17 @@ _msg_info() {
     _msg "\e[1;94m==>\e[1;0m ${1}"
 }
 
-_get_versions(){
-    pyenv versions
-}
-
-_set_version(){
-    export PYENV_VERSION="${@}"
-}
-
-_get_current(){
+_get_current_py(){
     version=$(pyenv version | awk '{print $1}' ORS=', ' | sed '$s/..$//')
     #bits=$(python -c 'import struct; print(struct.calcsize("P") * 8)')
     _msg_info "Current Python: ${version}" #(${bits} bits)"
 }
 
-_get_current
+_get_current_rb(){
+    version=$(rbenv version | awk '{print $1}' ORS=', ' | sed '$s/..$//')
+    #bits=$(python -c 'import struct; print(struct.calcsize("P") * 8)')
+    _msg_info "Current Ruby: ${version}" #(${bits} bits)"
+}
+
+_get_current_py
+_get_current_rb
