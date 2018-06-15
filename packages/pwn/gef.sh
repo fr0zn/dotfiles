5c90bda9c44f3332a1f49847c3432e4f65ed7cea
fr0zn <fr0zn@protonmail.com>
2018-06-15 22:01:19 +0200

    Moved ropper, gef install missing

--- packages/pwn/gef.sh
+++ packages/pwn/gef.sh
@@ -1,3 +1,11 @@
+pre_gef(){
+    program_must_exist pip3
+    sudo -H pip3 install capstone
+    sudo -H pip3 install filebytes
+    sudo -H pip3 install keystone-engine
+    sudo -H pip3 install ropper
+}
+
 install_gef(){
     clone_src https://github.com/hugsy/gef.git gef
     add_line "$HOME/.gdbinit" "source $DOTFILE_SRC/gef/gef.py"
