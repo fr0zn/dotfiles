aa65556f25d450abe4072a06d4631926cdb2d118
fr0zn <fr0zn@protonmail.com>
2018-06-15 17:45:29 +0200

    Updated pyenv script

--- scripts/pyenv_rbenv_version.sh
+++ scripts/pyenv_rbenv_version.sh
@@ -8,8 +8,13 @@ _msg_info() {
     _msg "\e[1;94m==>\e[1;0m ${1}"
 }
 
+_exists(){
+    command -v ${1} >/dev/null
+    return $?
+}
+
 _get_current_py(){
-    if [ -x "$(command -v pyenv)" ]; then
+    if _exists pyenv; then
         version=$(pyenv version | awk '{print $1}' ORS=', ' | sed '$s/..$//')
         #bits=$(python -c 'import struct; print(struct.calcsize("P") * 8)')
         _msg_info "Current Python: ${version}" #(${bits} bits)"
@@ -17,7 +22,7 @@ _get_current_py(){
 }
 
 _get_current_rb(){
-    if [ -x "$(command -v rbenv)" ]; then
+    if _exists rbenv; then
         version=$(rbenv version | awk '{print $1}' ORS=', ' | sed '$s/..$//')
         #bits=$(python -c 'import struct; print(struct.calcsize("P") * 8)')
         _msg_info "Current Ruby: ${version}" #(${bits} bits)"
@@ -25,7 +30,7 @@ _get_current_rb(){
 }
 
 _get_current_node(){
-    if [ -x "$(command -v nodenv)" ]; then
+    if _exists nodenv; then
         version=$(nodenv version | awk '{print $1}' ORS=', ' | sed '$s/..$//')
         #bits=$(python -c 'import struct; print(struct.calcsize("P") * 8)')
         _msg_info "Current Node: ${version}" #(${bits} bits)"
