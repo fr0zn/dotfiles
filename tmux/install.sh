6d2d04d18bbf47d9acf4569d8805399e5fbf615d
fr0zn <fr0zn@protonmail.com>
2018-06-16 09:33:35 +0200

    Checking for tmux already installed

--- tmux/install.sh
+++ tmux/install.sh
@@ -13,13 +13,19 @@ install_tmux(){
 install_tmux_ubuntu(){
     program_exists "tmux"
     if [[ "$?" != "0"  ]]; then
-        install_package "libevent-dev"
-        install_package "libncurses-dev"
+        version=$(tmux -V 2>/dev/null | grep 2.6)
+        if [[ "$?" != "0"  ]]; then
 
-        VERSION=2.6 && wget -qO- https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz | tar xvz -C $DOTFILE_SRC
-        cd $DOTFILE_SRC/tmux*
-        ./configure && make
-        sudo_run make install
+            sudo_run apt-get remove tmux # Uninstall repo tmux
+
+            install_package "libevent-dev"
+            install_package "libncurses-dev"
+
+            VERSION=2.6 && wget -qO- https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz | tar xvz -C $DOTFILE_SRC
+            cd $DOTFILE_SRC/tmux*
+            ./configure && make
+            sudo_run make install
+        fi
     fi
 }
 
