42f750bd81a44d351c5a4579132fcde55c76bf5a
fr0zn <fr0zn@protonmail.com>
2018-08-11 17:05:33 +0200

    Add function to change append or insert to path

---
+++ scripts/path.sh
@@ -0,0 +1,9 @@
+pathadd () {
+        if ! echo "$PATH" | /bin/grep -Eq "(^|:)$1($|:)" ; then
+           if [ "$2" = "after" ] ; then
+              PATH="$PATH:$1"
+           else
+              PATH="$1:$PATH"
+           fi
+        fi
+}
