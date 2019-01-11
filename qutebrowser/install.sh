eb4f838f485f8213529e3bb7f1dd28820fd3fd08
fr0zn <fr0zn@protonmail.com>
2019-01-11 12:30:11 +0100

    Adding tox to qutebrowser

--- qutebrowser/install.sh
+++ qutebrowser/install.sh
@@ -1,14 +1,14 @@
 pre_qutebrowser_ubuntu(){
     msg_debug "Pre-Install qutebrowser"
-    install_package libglib2.0-0 libgl1 libfontconfig1 libx11-xcb1 libxi6 libxrender1 libdbus-1-3
+    install_package libglib2.0-0 libgl1 libfontconfig1 libx11-xcb1 libxi6 libxrender1 libdbus-1-3 tox
 }
 
 install_qutebrowser_ubuntu(){
     msg_debug "Install qutebrowser"
     clone_src https://github.com/qutebrowser/qutebrowser.git qutebrowser
     pushd $DOTFILE_SRC/qutebrowser
-    # tox -e mkvenv-pypi
-    tox -e mkvenv-pypi-old
+    tox -e mkvenv-pypi
+    #tox -e mkvenv-pypi-old
 
 }
 
