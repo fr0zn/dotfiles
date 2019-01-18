e6687894c999943543c24a2aaf6400e294fede01
fr0zn <fr0zn@protonmail.com>
2019-01-18 18:17:04 +0100

    Adding dependencies

--- packages/pyenv.sh
+++ packages/pyenv.sh
@@ -3,6 +3,9 @@ install_pyenv_macos(){
 }
 
 install_pyenv_ubuntu(){
+    install_package make build-essential libssl-dev zlib1g-dev libbz2-dev \
+libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+xz-utils tk-dev libffi-dev liblzma-dev python-openssl
     curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
 }
 
