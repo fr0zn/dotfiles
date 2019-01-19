e209b8857c0c7a015865c767f909ffcbcfffc865
fr0zn <fr0zn@protonmail.com>
2019-01-19 15:15:38 +0100

    Fixing tmux ubuntu installation

--- tmux/install.sh
+++ tmux/install.sh
@@ -11,21 +11,15 @@ install_tmux(){
 }
 
 install_tmux_ubuntu(){
-    program_exists "tmux"
-    if [[ "$?" == "0"  ]]; then
-        version=$(tmux -V 2>/dev/null | grep 2.6)
-        if [[ "$?" != "0"  ]]; then
 
-            sudo_run apt-get remove tmux # Uninstall repo tmux
+    sudo_run apt-get remove tmux # Uninstall repo tmux
 
-            install_package "libevent-dev"
-            install_package "libncurses-dev"
+    install_package "libevent-dev"
+    install_package "libncurses-dev"
 
-            VERSION=2.6 && wget -qO- https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz | tar xvz -C $DOTFILE_SRC
-            cd $DOTFILE_SRC/tmux*
-            ./configure && make
-            sudo_run make install
-        fi
-    fi
+    VERSION=2.6 && wget -qO- https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz | tar xvz -C $DOTFILE_SRC
+    cd $DOTFILE_SRC/tmux*
+    ./configure && make
+    sudo_run make install
 }
 
