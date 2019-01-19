4512fda23552361247fbc9f4541a6c5c6d8864c0
fr0zn <fr0zn@protonmail.com>
2019-01-19 15:28:31 +0100

    Update pwnbox bootstrap

--- bootstrap/pwnbox.sh
+++ bootstrap/pwnbox.sh
@@ -1,4 +1,7 @@
 # Packages
+dpkg --add-architecture i386
+DB_SYNC=0
+
 install_package build-essential cmake
 install_package libc6:i386 libc6-dev-i386 libncurses5:i386 libstdc++6:i386 # libraries (32 bits)
 install_package libc6-dbg:i386 libc6-dbg
@@ -25,5 +28,7 @@ install fzf
 install peda
 install gef # will install ropper on python3
 install radare2
+install fixenv
+install libcdb
 install ropper
 install pwntools
