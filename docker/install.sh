c026ef1ea2949af913f64da9c2a9b5f9228030b5
fr0zn <fr0zn@protonmail.com>
2019-01-19 16:56:31 +0100

    Adding docker and pwnbox

---
+++ docker/install.sh
@@ -0,0 +1,8 @@
+install_docker_ubuntu(){
+    msg_debug "Install docker"
+    install_package install apt-transport-https ca-certificates curl gnupg2 software-properties-common curl
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    DB_SYNC=0
+    install_package docker-ce
+}
