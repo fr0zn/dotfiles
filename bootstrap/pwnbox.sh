21b67001a015f1b0a30adc588d61c53b55d04066
fr0zn <fr0zn@protonmail.com>
2019-01-19 15:42:16 +0100

    Adding curl

--- bootstrap/pwnbox.sh
+++ bootstrap/pwnbox.sh
@@ -7,7 +7,7 @@ install_package libc6:i386 libc6-dev-i386 libncurses5:i386 libstdc++6:i386 # lib
 install_package libc6-dbg:i386 libc6-dbg
 install_package gcc-multilib g++-multilib gcc-arm-none-eabi
 install_package gdb gdb-multiarch
-install_package socat netcat nmap net-tools wget ssh
+install_package socat netcat nmap net-tools wget ssh curl
 install_package nasm
 install_package ctags
 install_package binwalk exiftool
