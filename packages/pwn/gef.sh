27930d22069399c4109d61f06ff457f96fede7e6
fr0zn <fr0zn@protonmail.com>
2019-01-19 15:27:53 +0100

    Instal gef missings

--- packages/pwn/gef.sh
+++ packages/pwn/gef.sh
@@ -2,3 +2,10 @@ install_gef(){
     clone_src https://github.com/hugsy/gef.git gef
     echo "source $DOTFILE_SRC/gef/gef.py" > $DOTFILE_PATH/gdb/inits/gef.gdbinit
 }
+
+post_gef(){
+    sudo pip3 install unicorn
+    sudo pip3 install capstone
+    sudo pip3 install keystone-engine
+    sudo pip3 install ropper
+}
