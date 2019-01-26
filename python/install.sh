6bbd4916e86b9dc602b0ead9118583684f095671
fr0zn <fr0zn@protonmail.com>
2019-01-26 10:31:42 +0100

    python arch

--- python/install.sh
+++ python/install.sh
@@ -6,6 +6,14 @@ pyenv_python(){
     install pyenv
 }
 
+install_python_arch(){
+    install_package python
+    install_package python-pip
+
+    install_package python2
+    install_package python2-pip
+}
+
 install_python_ubuntu(){
     install_package python
     install_package python-pip
