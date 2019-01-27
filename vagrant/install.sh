65d27005b2b2634c07b6e943a7564c03b46efa55
fr0zn <fr0zn@protonmail.com>
2019-01-27 14:32:07 +0100

    vagrant arch

--- vagrant/install.sh
+++ vagrant/install.sh
@@ -1,3 +1,8 @@
+install_vagrant_arch(){
+    msg_debug "Install vagrant"
+    install_package virtualbox vagrant
+}
+
 install_vagrant_ubuntu(){
     msg_debug "Install vagrant"
     install_package virtualbox vagrant
