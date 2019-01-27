71204909ebdb729cdd925497dd3d13a76e16bd3f
fr0zn <fr0zn@protonmail.com>
2019-01-27 14:31:45 +0100

    Installing pip utils

--- packages/pwn/gef.sh
+++ packages/pwn/gef.sh
@@ -1,11 +1,12 @@
+pre_gef_ubuntu(){
+    install_package cmake pkg-config
+}
+
 install_gef(){
     clone_src https://github.com/hugsy/gef.git gef
     echo "source $DOTFILE_SRC/gef/gef.py" > $DOTFILE_PATH/gdb/inits/gef.gdbinit
 }
 
-post_gef(){
-    sudo pip3 install unicorn
-    sudo pip3 install capstone
-    sudo pip3 install keystone-engine
-    sudo pip3 install ropper
+post_gef_ubuntu(){
+    install piputils pre pip3
 }
---
+++ packages/pwn/piputils.sh
@@ -0,0 +1,17 @@
+pre_piputils_ubuntu(){
+    install_package cmake pkg-config
+}
+
+pip2_piputils_ubuntu(){
+    sudo pip install unicorn
+    sudo pip install capstone
+    sudo pip install keystone-engine
+    sudo pip install ropper
+}
+
+pip3_piputils_ubuntu(){
+    sudo pip3 install unicorn
+    sudo pip3 install capstone
+    sudo pip3 install keystone-engine
+    sudo pip3 install ropper
+}
