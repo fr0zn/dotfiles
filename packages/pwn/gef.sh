22bded621e0801a8498edd6ef63b265873921768
fr0zn <fr0zn@protonmail.com>
2019-01-27 14:40:34 +0100

    piputils pip3

--- packages/pwn/gef.sh
+++ packages/pwn/gef.sh
@@ -1,12 +1,9 @@
-pre_gef_ubuntu(){
-    install_package cmake pkg-config
-}
-
 install_gef(){
     clone_src https://github.com/hugsy/gef.git gef
     echo "source $DOTFILE_SRC/gef/gef.py" > $DOTFILE_PATH/gdb/inits/gef.gdbinit
 }
 
 post_gef_ubuntu(){
-    install piputils pre pip3
+    # ubuntu gdb uses python3 by default
+    install piputils pip3
 }
