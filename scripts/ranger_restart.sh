e4a3241746886a8008d253b39ba3a12fd3c0ab64
fr0zn <fr0zn@protonmail.com>
2019-02-01 19:50:43 +0100

    Ranger restart last path

--- scripts/ranger_restart.sh
+++ scripts/ranger_restart.sh
@@ -1,3 +1,13 @@
+
 while true; do
-    ranger
+    function ranger_cd {
+        tempfile="$(mktemp -t tmp.XXXXXX)"
+        ranger --choosedir="$tempfile" "${@:-$(pwd)}"
+        test -f "$tempfile" &&
+        if [ "$(cat -- "$tempfile")" != "$(echo -n `pwd`)" ]; then
+            cd -- "$(cat "$tempfile")"
+        fi
+        rm -f -- "$tempfile"
+    }
+    ranger_cd
 done
