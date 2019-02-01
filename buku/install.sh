6ef37eded72eb87107064c91ebf520dcc0dd1125
fr0zn <fr0zn@protonmail.com>
2019-02-01 19:29:57 +0100

    Buku arch

--- buku/install.sh
+++ buku/install.sh
@@ -3,6 +3,10 @@ symlink_buku(){
     symlink_path "buku/db" "$HOME/.local/share/buku"
 }
 
-install_buku(){
+install_buku_ubuntu(){
     install_package buku
+}
+
+install_buku_arch(){
+    install_aur buku
 }
