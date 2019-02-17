3d8defe7569864ea825d338971403295454485f2
fr0zn <ferran.celades@hotmail.com>
2019-02-17 15:56:08 +0100

    Adding skel command

---
+++ gdb/gef/skel.py
@@ -0,0 +1,54 @@
+import os
+
+template = '''#!/usr/bin/env python2
+from pwn import *
+import sys
+import distutils.spawn
+
+if distutils.spawn.find_executable('termite'):
+    context.terminal = ['termite', '-e']
+else:
+    context.terminal = ['tmux', 'splitw', '-h']
+
+DEBUG = False
+
+GDB_CMD = \'\'\'
+\'\'\'
+
+HOST = "{HOST}"
+PORT = {PORT}
+
+def exploit(p):
+    p.interactive()
+
+if __name__ == "__main__":
+
+    e = ELF("{BINARY}")
+
+    context.binary = "{BINARY}"
+
+    if '1' in sys.argv:
+        p = remote(HOST, PORT)
+    else:
+        if 'gdb' in sys.argv:
+            p = gdb.debug("{BINARY}", GDB_CMD)
+        else:
+            p = process("{BINARY}")
+
+    exploit(p)
+'''
+
+class Skeleton(GenericCommand):
+    """Dummy new command."""
+    _cmdline_ = "skel"
+    _syntax_  = "{:s}".format(_cmdline_)
+
+    def do_invoke(self, argv):
+
+        if len(argv) < 2:
+            err("Invalid number of arguments")
+            return
+
+        print template.format(HOST=argv[0], PORT=argv[1], BINARY=get_filename())
+
+register_external_command(Skeleton())
--- packages/pwn/gef.sh
+++ packages/pwn/gef.sh
@@ -1,6 +1,7 @@
 install_gef(){
     clone_src https://github.com/hugsy/gef.git gef
     echo "source $DOTFILE_SRC/gef/gef.py" > $DOTFILE_PATH/gdb/inits/gef.gdbinit
+    echo "source $DOTFILE_PATH/gdb/gef/skel.py" >> $DOTFILE_PATH/gdb/inits/gef.gdbinit
 }
 
 post_gef_ubuntu(){
