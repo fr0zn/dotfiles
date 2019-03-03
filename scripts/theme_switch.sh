4f2254438d526613f331da2e852cf4138c87734d
fr0zn <fr0zn@protonmail.com>
2019-03-03 09:27:01 +0100

    Theme switcher

--- scripts/theme_switch.sh
+++ scripts/theme_switch.sh
@@ -5,13 +5,19 @@ theme-switch () {
 
     set_dark="true"
     if [[ "$uname_out" == "Darwin" ]]; then
-        echo -e "\033]50;SetProfile=$1\a"; export ITERM_PROFILE=$1;
+        echo -e "\033]50;SetProfile=$1\a"; export TERM_PROFILE=$1;
         #if  [ "$1" = "light" ]; then
             #set_dark="false"
         #fi
         #/usr/bin/osascript -e "tell application \"System Events\" to tell appearance preferences to set dark mode to $set_dark"
     elif [[ "$uname_out" == "Linux" ]]; then
-        echo "TODO"
+        theme=$HOME/.dotfiles/termite/themes/$1
+        if [ -f $theme ]; then
+            unlink $HOME/.dotfiles/termite/config 2>/dev/null
+            ln -s $theme $HOME/.dotfiles/termite/config
+            killall -USR1 termite
+            export TERM_PROFILE=$1
+        fi
     fi
 }
 
