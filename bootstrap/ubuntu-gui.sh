2c28f9f79935ca9567b70edd947d4e7835e0e703
fr0zn <fr0zn@protonmail.com>
2019-04-20 20:08:52 +0200

    Ubuntu GUI minimal

---
+++ bootstrap/ubuntu-gui.sh
@@ -0,0 +1,21 @@
+# Packages
+sudo_run dpkg --add-architecture i386
+install_package build-essential
+install_package libc6:i386 libncurses5:i386 libstdc++6:i386 # libraries (32 bits)
+install_package gcc-multilib # compile 32 bits
+install_package libc6-dbg:i386 # libc symbols 32
+install_package libc6-dbg # libc symbols 64
+
+install_package fonts-hack-ttf
+
+# GUI and terminal
+install i3 install symlink-min
+install termite
+
+install bash
+
+# Tools
+install_package gdb gdb-multiarch
+
+# GUI apps
+install chrome
