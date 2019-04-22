7875888e5b1cf1c61013565e25fc2fba96fd2e78
fr0zn <ferran.celades@hotmail.com>
2019-04-22 14:00:42 +0200

    Force vim install

--- vim/install.sh
+++ vim/install.sh
@@ -10,7 +10,7 @@ install_vim_ubuntu(){
     install_package software-properties-common
     sudo_run add-apt-repository -y ppa:jonathonf/vim
     sudo_run apt-get update
-    sudo_run apt-get install vim
+    sudo_run apt-get install -y vim
 }
 
 install_vim(){
