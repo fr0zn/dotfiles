fee0eb4d26ca8aeafc0fa29c02a20ca3d26dcc99
fr0zn <ferran.celades@hotmail.com>
2019-04-22 14:03:32 +0200

    Updated bootstraps

--- bootstrap/ubuntu-pwn-gui.sh
+++ bootstrap/ubuntu-pwn-gui.sh
@@ -1,13 +1,13 @@
 # Packages
 sudo_run dpkg --add-architecture i386
-sudo_run apt-get update
+install_package build-essential
+install_package libc6:i386 libncurses5:i386 libstdc++6:i386
+install_package gcc-multilib
+install_package libc6-dbg:i386
+install_package libc6-dbg
 
-# Utilities
-install_package build-essential cmake
-install_package libc6:i386 libc6-dev-i386 libncurses5:i386 libstdc++6:i386 # libraries (32 bits)
-install_package libc6-dbg:i386 libc6-dbg
-install_package gcc-multilib g++-multilib
 install_package gdb gdb-multiarch
+
 install_package socat netcat nmap net-tools wget ssh curl
 install_package nasm
 install_package ctags
@@ -15,28 +15,30 @@ install_package binwalk exiftool
 install_package strace ltrace
 install_package unzip
 
-# ARM
-install_package gcc-arm-linux-gnueabihf gcc-aarch64-linux-gnu
-# arm 32
-install_package libc6-dbg-armhf-cross libc6-dev-armhf-cross
-# arm 64
-install_package libc6-dbg-arm64-cross libc6-dev-arm64-cross
-install_package qemu qemu-user qemu-user-static
-
 install python
 install python dev
+
+# ARM
+install_package gcc-arm-linux-gnueabihf gcc-aarch64-linux-gnu
+install_package libc6-dbg-armhf-cross libc6-dev-armhf-cross
+install_package libc6-dbg-arm64-cross libc6-dev-arm64-cross
+install_package qemu qemu-user qemu-user-static
 
 install vim
 install tmux
 
-install bash
-
-install fzf
-
-install peda
+# pwn
 install gef
 install piputils pip3 # will install unicorn, capstone, keystone-engine, ropper
 install radare2
-install fixenv
 install libcdb
 install pwntools
+
+# GUI and terminal
+install i3 install symlink-min
+install termite
+install fzf
+install bash
+install_package fonts-hack-ttf
+# GUI apps
+install chrome
