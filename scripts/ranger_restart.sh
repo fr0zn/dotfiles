36509055b696ad8c63097027eeea3d9405228c3f
fr0zn <fr0zn@protonmail.com>
2019-05-06 16:30:00 +0200

    Ranger dual panel

--- scripts/ranger_restart.sh
+++ scripts/ranger_restart.sh
@@ -2,7 +2,7 @@
 while true; do
     ranger_cd () {
         tempfile="$(mktemp -t tmp.XXXXXX)"
-        ranger --choosedir="$tempfile" "${@:-$(pwd)}"
+        ranger --choosedir="$tempfile" "${@:-$(pwd)}" "${@:-$(pwd)}"
         test -f "$tempfile" &&
         if [ "$(cat -- "$tempfile")" != "$(echo -n `pwd`)" ]; then
             cd -- "$(cat "$tempfile")"
