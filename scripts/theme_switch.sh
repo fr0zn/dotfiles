f5f558857205992e89de90777f2af77a2f7abd53
fr0zn <ferran.celades@hotmail.com>
2019-05-09 13:30:56 +0200

    Update

--- scripts/theme_switch.sh
+++ scripts/theme_switch.sh
@@ -3,16 +3,17 @@
 theme-switch () {
     uname_out=`uname`
 
-    set_dark="true"
     if [[ "$uname_out" == "Darwin" ]]; then
         #echo -e "\033]50;SetProfile=$1\a";
         echo $1 > $HOME/.dotfiles/theme
         if  [ "$1" = "light" ]; then
             osascript -e 'tell application "System Events" to key code 99 using {command down}'
+            set_dark="false"
         else
             osascript -e 'tell application "System Events" to key code 118 using {command down}'
+            set_dark="true"
         fi
-        #/usr/bin/osascript -e "tell application \"System Events\" to tell appearance preferences to set dark mode to $set_dark"
+        /usr/bin/osascript -e "tell application \"System Events\" to tell appearance preferences to set dark mode to $set_dark"
     elif [[ "$uname_out" == "Linux" ]]; then
         theme=$HOME/.dotfiles/termite/themes/$1
         if [ -f $theme ]; then
