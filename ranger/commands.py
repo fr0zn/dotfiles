3cba9db5129cc45b545cf043024d99b556eb2f46
fr0zn <ferran.celades@hotmail.com>
2019-08-22 10:05:15 +0200

    Update commands.py

--- ranger/commands.py
+++ ranger/commands.py
@@ -548,27 +548,26 @@ class fd_prev(Command):
             self.fm.select_file(fd_deq[0])
             
             
-class fzf_rga_documents_search(Command):
+class rg(Command):
     """
-    :fzf_rga_search_documents
-    Search in PDFs, E-Books and Office documents in current directory.
-    Allowed extensions: .epub, .odt, .docx, .fb2, .ipynb, .pdf.
-
-    Usage: fzf_rga_search_documents <search string>
+    :rg
+    Usage: rg <search string>
     """
     def execute(self):
         if self.arg(1):
             search_string = self.rest(1)
         else:
-            self.fm.notify("Usage: fzf_rga_search_documents <search string>", bad=True)
+            self.fm.notify("Usage: rg <search string>", bad=True)
             return
 
         import subprocess
         import os.path
         from ranger.container.file import File
-        command="rga '%s' . --rga-adapters=pandoc,poppler | fzf +m | awk -F':' '{print $1}'" % search_string
+        command="rg '%s' | fzf +m | awk -F':' '{print $1}'" % search_string
         fzf = self.fm.execute_command(command, universal_newlines=True, stdout=subprocess.PIPE)
         stdout, stderr = fzf.communicate()
         if fzf.returncode == 0:
             fzf_file = os.path.abspath(stdout.rstrip('\n'))
-            self.fm.execute_file(File(fzf_file))
+            self.fm.select_file(fzf_file)
+            # self.fm.execute_file(File(fzf_file))
+
