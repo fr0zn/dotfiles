0247a8b66284d68e734f4e293f55e73491ef3c7d
fr0zn <ferran.celades@hotmail.com>
2020-04-29 20:27:14 +0200

    Fixed sudo check

--- bootstrap.sh
+++ bootstrap.sh
@@ -66,10 +66,10 @@ lnif() {
 has_sudo() {
     local prompt
 
-    prompt=$(sudo -nv 2>&1)
+    prompt=$(sudo -n true 2>&1)
     if [ $? -eq 0 ]; then
     echo "has_sudo__pass_set"
-    elif echo $prompt | grep -q '^sudo:'; then
+    elif echo $prompt | grep -q 'required'; then
     echo "has_sudo__needs_pass"
     else
     echo "no_sudo"
