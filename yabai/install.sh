0fc923dfa0eef0ad022574f675aa8ad3d28f7141
fr0zn <ferran.celades@hotmail.com>
2020-05-06 15:34:46 +0200

    Updating yabai, using spacebar

--- yabai/install.sh
+++ yabai/install.sh
@@ -8,9 +8,17 @@ symlink_yabai_macos(){
 
 install_yabai_macos(){
     brew tap koekeishiya/formulae
-    brew install --HEAD yabai
+    brew install yabai
 }
 
 post_yabai_macos(){
     brew services restart yabai
+}
+
+bar_yabai_macos(){
+    brew install --HEAD somdoron/formulae/spacebar
+    clean mkdir -p ~/.config/spacebar/
+    symlink_file "yabai/spacebarrc" "$HOME/.config/spacebar/spacebarrc"
+    chmod +x "$HOME/.config/spacebar/spacebarrc"
+    brew services restart spacebar
 }
