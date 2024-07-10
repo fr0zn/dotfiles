ae38c27a1581cef74ae471f6512f632fea9b4fce
Ferran Celades <ferran.celades@hotmail.com>
2024-07-10 09:18:52 +0200

    goenv roots

--- shells/env.sh
+++ shells/env.sh
@@ -45,6 +45,8 @@ _get_current_node(){
 
 _get_current_go(){
     if _exists goenv; then
+        export GOENV_ROOT="$HOME/.goenv"
+        export PATH="$GOENV_ROOT/bin:$PATH"
         eval "$(goenv init -)"
         export PATH="$GOROOT/bin:$PATH"
         export PATH="$PATH:$GOPATH/bin"
